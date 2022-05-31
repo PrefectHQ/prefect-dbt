9df5ea7c38c5b228ed4f24a708d2011caad59299
Alexander Streed <alex.s@prefect.io>
2022-05-31 16:09:42 -0500

    Adds examples to trigger_job_run task docstring

--- prefect_dbt/cloud/jobs.py
+++ prefect_dbt/cloud/jobs.py
@@ -31,7 +31,62 @@ async def trigger_job_run(
 
     Returns:
         The response from the dbt Cloud administrative API.
-    """
+
+    Example:
+        Trigger a dbt Cloud job run:
+        ```python
+        from prefect import flow
+
+        from prefect_dbt.cloud import DbtCloudCredentials
+        from prefect_dbt.cloud.jobs import trigger_job_run
+
+
+        @flow
+        def trigger_job_run_flow():
+            credentials = DbtCloudCredentials(api_key="my_api_key", account_id=123456789)
+
+            trigger_job_run(dbt_cloud_credentials=credentials, job_id=1)
+
+
+        trigger_job_run_flow()
+        ```
+
+        Trigger a dbt Cloud job run with overrides:
+        ```python
+        from prefect import flow
+
+        from prefect_dbt.cloud import DbtCloudCredentials
+        from prefect_dbt.cloud.jobs import trigger_job_run
+        from prefect_dbt.cloud.models import TriggerJobRunOptions
+
+
+        @flow
+        def trigger_job_run_flow():
+            credentials = DbtCloudCredentials(api_key="my_api_key", account_id=123456789)
+
+            trigger_job_run(
+                dbt_cloud_credentials=credentials,
+                job_id=1,
+                options=TriggerJobRunOptions(
+                    git_branch="staging",
+                    schema_override="dbt_cloud_pr_123",
+                    dbt_version_override="0.18.0",
+                    target_name_override="staging",
+                    timeout_seconds_override=3000,
+                    generate_docs_override=True,
+                    threads_override=8,
+                    steps_override=[
+                        "dbt seed",
+                        "dbt run --fail-fast",
+                        "dbt test --fail fast",
+                    ],
+                ),
+            )
+
+
+        trigger_job_run_flow()
+        ```
+    """  # noqa
     try:
         async with dbt_cloud_credentials.get_administrative_client() as client:
             response = await client.trigger_job_run(job_id=job_id, options=options)
