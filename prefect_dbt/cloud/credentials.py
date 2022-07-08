8485b2533f0e2cca39f6387ecb6a019c26051b87
Alexander Streed <alex.s@prefect.io>
2022-07-08 16:34:07 -0500

    Converts DbtCloudCredentials to a block

--- prefect_dbt/cloud/credentials.py
+++ prefect_dbt/cloud/credentials.py
@@ -1,11 +1,11 @@
 """Module containing credentials for interacting with dbt Cloud"""
-from dataclasses import dataclass
+from prefect.blocks.core import Block
+from pydantic import SecretStr
 
 from prefect_dbt.cloud.clients import DbtCloudAdministrativeClient
 
 
-@dataclass
-class DbtCloudCredentials:
+class DbtCloudCredentials(Block):
     """
     Credentials class for credential use across dbt Cloud tasks and flows.
 
@@ -24,9 +24,35 @@ class DbtCloudCredentials:
         async with dbt_cloud_credentials.get_administrative_client() as client:
             client.trigger_job_run(job_id=1)
         ```
+
+        Load saved dbt Cloud credentials within a flow:
+        ```python
+        from prefect import flow
+
+        from prefect_dbt.cloud import DbtCloudCredentials
+        from prefect_dbt.cloud.jobs import trigger_job_run
+
+
+        @flow
+        def trigger_job_run_flow():
+            credentials = DbtCloudCredentials.load("my-dbt-credentials")
+
+            trigger_job_run(dbt_cloud_credentials=credentials, job_id=1)
+
+
+        trigger_job_run_flow()
+        ```
     """
 
-    api_key: str
+    _block_type_name = "dbt Cloud Credentials"
+    _code_example = """/
+    ```python
+        from prefect_dbt.cloud import DbtCloudCredentials
+
+        dbt_cloud_credentials = DbtCloudCredentials.load("BLOCK_NAME")
+    ```"""
+
+    api_key: SecretStr
     account_id: int
     domain: str = "cloud.getdbt.com"
 
@@ -36,5 +62,7 @@ class DbtCloudCredentials:
         administrative API.
         """
         return DbtCloudAdministrativeClient(
-            api_key=self.api_key, account_id=self.account_id, domain=self.domain
+            api_key=self.api_key.get_secret_value(),
+            account_id=self.account_id,
+            domain=self.domain,
         )
