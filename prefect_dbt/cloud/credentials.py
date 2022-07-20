e309d1e852cd30769d63968018b7069cbc4d74af
Isabela Sobral <35778239+belasobral93@users.noreply.github.com>
2022-07-20 10:48:21 -0700

    Updated prefect_dbt.cloud.jobs import
    Updated prefect_dbt.cloud.jobs import to reflect accurate name for prefect_dbt.cloud.jobs import()

--- prefect_dbt/cloud/credentials.py
+++ prefect_dbt/cloud/credentials.py
@@ -31,7 +31,7 @@ class DbtCloudCredentials(Block):
         from prefect import flow
 
         from prefect_dbt.cloud import DbtCloudCredentials
-        from prefect_dbt.cloud.jobs import trigger_job_run
+        from prefect_dbt.cloud.jobs import trigger_dbt_cloud_job_run
 
 
         @flow
