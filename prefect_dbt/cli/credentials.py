080f31d03520f21d8733d6d51cd11d0f3afcf036
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-10-10 12:35:26 -0700

    Fix example in credentials (#78)
    * Fix example in credentials
    
    Walkthrough how to use prefect dbt CLI and found this example to be outdated.
    
    * Update credentials.py

--- prefect_dbt/cli/credentials.py
+++ prefect_dbt/cli/credentials.py
@@ -37,18 +37,22 @@ class DbtCliProfile(Block):
         from prefect_dbt.cli import DbtCliProfile
         from prefect_dbt.cli.configs import SnowflakeTargetConfigs
         from prefect_snowflake.credentials import SnowflakeCredentials
+        from prefect_snowflake.database import SnowflakeConnector
 
-        snowflake_credentials = SnowflakeCredentials(
-            schema="schema",
+        credentials = SnowflakeCredentials(
             user="user",
             password="password",
-            account="account",
+            account="account.region.aws",
             role="role",
+        )
+        connector = SnowflakeConnector(
+            schema="public",
             database="database",
             warehouse="warehouse",
+            credentials=credentials,
         )
         target_configs = SnowflakeTargetConfigs(
-            credentials=snowflake_credentials
+            connector=connector
         )
         dbt_cli_profile = DbtCliProfile(
             name="jaffle_shop",
