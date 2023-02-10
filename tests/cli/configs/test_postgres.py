1c772547c7670ba827b4be311133b4baca2d3098
ahuang11 <andrew.h@prefect.io>
2023-02-10 12:22:30 -0800

    Address comments

--- tests/cli/configs/test_postgres.py
+++ tests/cli/configs/test_postgres.py
@@ -46,7 +46,9 @@ def test_postgres_target_configs_get_configs_for_sqlalchemy_connector():
                 password="prefect_password",
                 host="host",
                 port=8080,
-            )
+                query={"a": "query"}
+            ),
+            kwarg_that_shouldnt_show_up=True
         ),
         schema="schema",
         extras={"retries": 1},
@@ -67,3 +69,7 @@ def test_postgres_target_configs_get_configs_for_sqlalchemy_connector():
         actual_v = v.get_secret_value() if isinstance(v, SecretStr) else v
         expected_v = expected[k]
         assert actual_v == expected_v
+    assert hasattr(configs.credentials, "kwarg_that_shouldnt_show_up")
+    assert "kwarg_that_shouldnt_show_up" not in actual.keys()
+    assert hasattr(configs.credentials.connection_info, "query")
+    assert "query" not in actual.keys()
