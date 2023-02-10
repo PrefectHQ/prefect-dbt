b2baf69ef468560fa50b29975c24dcbb112fd171
ahuang11 <andrew.h@prefect.io>
2023-02-10 15:35:08 -0800

    Fix windows test

--- tests/cli/test_commands.py
+++ tests/cli/test_commands.py
@@ -281,6 +281,6 @@ class TestDbtCoreOperation:
             op.run()
 
         mock_write = mock_named_temporary_file.return_value.__enter__.return_value.write
-        mock_write.assert_called_once_with(
+        mock_write.assert_called_with(
             f"dbt debug --profiles-dir {tmp_path} --project-dir {tmp_path}".encode()
         )
